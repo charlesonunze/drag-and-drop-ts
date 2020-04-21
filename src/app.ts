1808f67b574bb00b2dabba653a4b93de7160709b
charlesonunze <charlesonunze2@gmail.com>
2020-04-21 09:10:55 +0100

    add elements and event listeners

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,42 @@
 class ProjectInput {
-	template: HTMLTemplateElement;
-	form: HTMLFormElement;
-	app: HTMLDivElement;
+	_template: HTMLTemplateElement;
+	_form: HTMLFormElement;
+	_app: HTMLDivElement;
+
+	_description: HTMLInputElement;
+	_people: HTMLInputElement;
+	_title: HTMLInputElement;
 
 	constructor() {
-		this.app = document.getElementById('app') as HTMLDivElement;
-		this.template = document.getElementById(
+		this._app = document.getElementById('app') as HTMLDivElement;
+		this._template = document.getElementById(
 			'project-input'
 		) as HTMLTemplateElement;
 
-		const importedNode = document.importNode(this.template.content, true);
-		this.form = importedNode.firstElementChild as HTMLFormElement;
+		const importedNode = document.importNode(this._template.content, true);
+		this._form = importedNode.firstElementChild as HTMLFormElement;
+		this._form.id = 'user-input';
 
+		this._title = this._form.querySelector('#title') as HTMLInputElement;
+		this._people = this._form.querySelector('#people') as HTMLInputElement;
+		this._description = this._form.querySelector(
+			'#description'
+		) as HTMLInputElement;
+
+		this.addListener();
 		this.attachNode();
 	}
 
+	private submitHandler(event: Event) {
+		event.preventDefault();
+	}
+
+	private addListener() {
+		this._form.addEventListener('submit', this.submitHandler.bind(this));
+	}
+
 	private attachNode() {
-		this.app.insertAdjacentElement('afterbegin', this.form);
+		this._app.insertAdjacentElement('afterbegin', this._form);
 	}
 }
 
