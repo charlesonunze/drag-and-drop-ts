661eecf7025f8c5f8b8beacef4b5a569ef5f6b0a
charlesonunze <charlesonunze2@gmail.com>
2020-04-22 12:46:40 +0100

    get user input

--- src/app.ts
+++ src/app.ts
@@ -40,9 +40,35 @@ class ProjectInput {
 		this.attachNode();
 	}
 
+	private getUserInput(): [string, string, number] | void {
+		const _title = this._title.value;
+		const _description = this._description.value;
+		const _people = parseInt(this._people.value);
+
+		if (!_title || !_description || !_people) {
+			console.log('Please enter valid information');
+			return;
+		}
+
+		return [_title, _description, _people];
+	}
+
+	private clearUserInput() {
+		this._title.value = '';
+		this._people.value = '';
+		this._description.value = '';
+	}
+
 	@BindThisToThis
 	private submitHandler(event: Event) {
 		event.preventDefault();
+		const data = this.getUserInput();
+
+		if (Array.isArray(data)) {
+			const [_title, _description, _people] = data;
+			console.log(_title, _description, _people);
+			this.clearUserInput();
+		}
 	}
 
 	private addListener() {
